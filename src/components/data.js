d29d80c4c61135a58d5c32499ab6abfb67e2719e
yahya momin <yahyamomin06@gmail.com>
2024-01-08 13:42:17 +0530

    update

--- src/components/data.js
+++ src/components/data.js
@@ -1,80 +1,80 @@
-import clineHub from "../assets/clinehub.vercel.app_(Nest Hub Max) (1).png";
-import agecal from "../assets/Screen Shot 2023-07-30 at 13.57.46.png";
-import captcha from "../assets/Screen Shot 2023-07-30 at 13.58.21.png";
-import gradient from "../assets/Screen Shot 2023-07-30 at 13.59.13.png";
-import validation from "../assets/Screen Shot 2023-07-30 at 14.00.18.png";
-import weather from "../assets/Screen Shot 2023-07-30 at 14.00.55.png";
-import aniHub from "../assets/ani-hub-rouge.vercel.app_(Nest Hub Max).png";
-import YtClone from "../assets/youtube-clone-tawny-omega.vercel.app_(Nest Hub Max) (2).png";
-import FinstaMiles from "../assets/Screen Shot 2023-12-04 at 12.42.12.png";
+import clineHub from '../assets/clinehub.vercel.app_(Nest Hub Max) (1).png'
+import agecal from '../assets/Screen Shot 2023-07-30 at 13.57.46.png'
+import captcha from '../assets/Screen Shot 2023-07-30 at 13.58.21.png'
+import gradient from '../assets/Screen Shot 2023-07-30 at 13.59.13.png'
+import validation from '../assets/Screen Shot 2023-07-30 at 14.00.18.png'
+import weather from '../assets/Screen Shot 2023-07-30 at 14.00.55.png'
+import aniHub from '../assets/ani-hub-rouge.vercel.app_(Nest Hub Max).png'
+import YtClone from '../assets/youtube-clone-tawny-omega.vercel.app_(Nest Hub Max) (2).png'
+import FinstaMiles from '../assets/Screen Shot 2023-12-04 at 12.42.12.png'
 
 const data = [
   {
-    name: "FinstaMiles",
+    name: 'FinstaMiles',
     image: FinstaMiles,
     description:
-      "Unleash the Power of Connection and Expression in a Seamless MERN Experience.  Welcome to FinstaMiles, where the world comes together through the art of genuine connection! Our MERN (MongoDB, Express.js, React.js, Node.js) stack social media app redefines the way you engage with others, providing a platform that seamlessly combines innovation and user-friendly design. ",
-    liveLink: "https://finstamiles.onrender.com/",
-    githubLink: "https://github.com/yahyaMomin/fistaMiles_client",
+      'Unleash the Power of Connection and Expression in a Seamless MERN Experience.  Welcome to FinstaMiles, where the world comes together through the art of genuine connection! Our MERN (MongoDB, Express.js, React.js, Node.js) stack social media app redefines the way you engage with others, providing a platform that seamlessly combines innovation and user-friendly design. ',
+    liveLink: 'https://finstamiles.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/fistaMiles_client',
   },
   {
-    name: "clinehub",
+    name: 'clinehub',
     image: clineHub,
     description:
-      "The ClineHub is Website That allow user to See any Movie or a TV shows ratting or information this Website is using TMDB api for fatching Movies and TV shows data",
-    liveLink: "https://clinehub.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/ClineHub",
+      'The ClineHub is Website That allow user to See any Movie or a TV shows ratting or information this Website is using TMDB api for fatching Movies and TV shows data',
+    liveLink: 'https://clinehub.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/ClineHub',
   },
   {
-    name: "Youtube Clone",
+    name: 'Youtube Clone',
     image: YtClone,
-    description: "Youtube clone using react and material UI fully responsive",
-    liveLink: "https://youtube-clone-tawny-omega.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/Youtube-clone",
+    description: 'Youtube clone using react and material UI fully responsive',
+    liveLink: 'https://youtube-clone-tawny-omega.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/Youtube-clone',
   },
   {
-    name: "aniHub",
+    name: 'aniHub',
     image: aniHub,
     description:
-      "The Anihub is large anime database which is provides user to see anime or a manga details this website is uses jikan unofficial api of myAnimeList api for fatching anime or manga data",
-    liveLink: "https://ani-hub-rouge.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/AniHub",
+      'The Anihub is large anime database which is provides user to see anime or a manga details this website is uses jikan unofficial api of myAnimeList api for fatching anime or manga data',
+    liveLink: 'https://ani-hub-rouge.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/AniHub',
   },
   {
-    name: "Age calculator",
+    name: 'Age calculator',
     image: agecal,
-    description: "simple age calculator using HTML CSS and JAVASCRIPT",
-    liveLink: "https://age-calculator-nine-zeta.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/Age-calculator",
+    description: 'simple age calculator using HTML CSS and JAVASCRIPT',
+    liveLink: 'https://age-calculator-nine-zeta.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/Age-calculator',
   },
   {
-    name: "Weather App",
+    name: 'Weather App',
     image: weather,
-    description: "weather app using  HTML CSS and JAVASCRIPT",
-    liveLink: "https://weather-app-phi-ten-31.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/weather-app",
+    description: 'weather app using  HTML CSS and JAVASCRIPT',
+    liveLink: 'https://weather-app-phi-ten-31.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/weather-app',
   },
   {
-    name: "captcha Generator",
+    name: 'captcha Generator',
     image: captcha,
-    description: "Simple captcha generator for authentication",
-    liveLink: "https://javascript-captcha-generator.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/javascript_captcha_generator",
+    description: 'Simple captcha generator for authentication',
+    liveLink: 'https://javascript-captcha-generator.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/javascript_captcha_generator',
   },
   {
-    name: "Password Validation",
+    name: 'Password Validation',
     image: validation,
-    description: "how strong is your password ? ",
-    liveLink: "https://form-validation-chi-six.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/Form-validation",
+    description: 'how strong is your password ? ',
+    liveLink: 'https://form-validation-chi-six.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/Form-validation',
   },
   {
-    name: "gradient generator",
+    name: 'gradient generator',
     image: gradient,
-    description: "get best random or pick gradient",
-    liveLink: "https://javascript-captcha-generator-sdy5.vercel.app/",
-    githubLink: "https://github.com/yahyaMomin/gradient-generator",
+    description: 'get best random or pick gradient',
+    liveLink: 'https://javascript-captcha-generator-sdy5.vercel.app/',
+    githubLink: 'https://github.com/yahyaMomin/gradient-generator',
   },
-];
+]
 
-export default data;
+export default data
